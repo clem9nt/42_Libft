f27f9a335503a64b9f68d1b449d6daabd7149797
Clément VIDON <cvidon@student.42.fr>
2022-03-16 16:07:12 +0100

    Update ft_lstclear ft_lstmap

--- src/linked_list/ft_lstmap.c
+++ src/linked_list/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: cvidon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 10:51:05 by cvidon            #+#    #+#             */
-/*   Updated: 2021/11/30 17:36:44 by cvidon           ###   ########.fr       */
+/*   Updated: 2022/03/16 16:06:44 by cvidon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		tmp = ft_lstnew((*f)(cpy->content));
 		if (!tmp)
 		{
-			ft_lstclear(&tmp, del);
-			ft_lstclear(&new, del);
+			ft_lstclear(tmp, del);
+			ft_lstclear(new, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&new, tmp);
