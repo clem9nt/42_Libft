e8578109533494dc330879b8ec35ae01232e25a3
Clément VIDON <cvidon@student.42.fr>
2022-03-17 12:55:04 +0100

    Update libft.h ft_lstclear.c

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: cvidon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:50:48 by cvidon            #+#    #+#             */
-/*   Updated: 2022/03/16 16:06:32 by cvidon           ###   ########.fr       */
+/*   Updated: 2022/03/17 12:53:34 by cvidon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,7 +105,7 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **alst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
-void	ft_lstclear(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
--- src/linked_list/ft_lstclear.c
+++ src/linked_list/ft_lstclear.c
@@ -6,21 +6,21 @@
 /*   By: cvidon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 15:39:23 by cvidon            #+#    #+#             */
-/*   Updated: 2022/03/16 16:06:26 by cvidon           ###   ########.fr       */
+/*   Updated: 2022/03/17 12:53:19 by cvidon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstclear(t_list *lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*ptr;
 
-	ptr = lst;
+	ptr = *lst;
 	while (ptr != NULL)
 	{
-		lst = lst->next;
+		*lst = (*lst)->next;
 		ft_lstdelone(ptr, del);
-		ptr = lst;
+		ptr = *lst;
 	}
 }
