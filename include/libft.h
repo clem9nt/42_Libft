2a9c59882488db574cdff4e92115e0f88200f7d7
Clément VIDON <cvidon@student.42.fr>
2022-04-19 16:19:48 +0200

    Add atol for pushswap

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: cvidon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:50:48 by cvidon            #+#    #+#             */
-/*   Updated: 2022/04/15 12:11:58 by cvidon           ###   ########.fr       */
+/*   Updated: 2022/04/19 16:19:21 by cvidon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,7 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_atoi(const char *str);
+long	ft_atol(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
